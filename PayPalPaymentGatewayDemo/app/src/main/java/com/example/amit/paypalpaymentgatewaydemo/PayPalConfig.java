4e6f1e33272c59d494cfc5d3735996c82b5a9347
Amit Kumar Sahoo <sahoo.kumar.amit@gmail.com>
2018-01-14 19:18:11 +0530

    Payment gateways
    Paypal paymentGateway and Paytm PaymentGateway

---
+++ PayPalPaymentGatewayDemo/app/src/main/java/com/example/amit/paypalpaymentgatewaydemo/PayPalConfig.java
@@ -0,0 +1,11 @@
+package com.example.amit.paypalpaymentgatewaydemo;
+
+/**
+ * Created by Amit on 13-01-2018.
+ */
+
+public class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AeB0m-_GqhWAld87cVcqMzlBWTnQnj1HAbxiGipU3ht4aqERMrGzaPMpk74c-kqzg4o-7lZsYujnY2cb";
+
+}
